097b7248cb6262f6e23d4b00c90db6ca09e0339e
CibelleN94 <belles2.n@gmail.com>
2025-04-11 13:46:44 -0300

    Relogio atualizado

---
+++ relogio.js
@@ -0,0 +1,14 @@
+function atualizarHora(){
+
+
+
+var display = document.querySelector('.display')
+var agora = new Date();
+var hora = agora.getHours() + ':' + agora.getMinutes() + ':' + agora.getSeconds();
+
+
+
+display.textContent = hora
+}
+
+setInterval(atualizarHora,1000);
